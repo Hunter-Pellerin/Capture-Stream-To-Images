0eeb1dd77644ff6c64baf088ca6129c1e29962ad
Hunter-Pellerin <hpellerin2003@gmail.com>
2020-01-10 21:52:20 -0500

    added output dir & tweaked processing algorithm

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ native_fps = 30
 cap_res = (640,480) # 480p works best
 scaled_res = (224,224) # default for Jetson Nano
 
-output_folder_name = "classification_imgs"
+output_folder_name = "thumbs_B"
+output_directory = "" # leave blank to use a new folder in the current directory
--- separateframes.py
+++ separateframes.py
@@ -1,36 +1,39 @@
-import cv2
-import os
-import sys
-
+import cv2, os, sys
 import config
 
 cwd = os.getcwd()
 path = os.path.join(cwd, config.output_folder_name)
 
 def setDir():
-    try:
-        os.chdir(path)
-    except:
-        os.mkdir(path)
-        os.chdir(path)
+    if config.output_directory != "":
+        path == config.output_directory
+        return True
+    else:
+        try:
+            os.chdir(path)
+        except:
+            os.mkdir(path)
+            os.chdir(path)
+        return True
+
 
 def getFrames():
-    totalFrames = config.native_fps
-    frameIndex = 0 # uses the arbitrary value for desired amount of output fps
-    resizeCount = 0
+    total_frames = config.native_fps
+    frame_index = 0 # uses the arbitrary value for desired amount of output fps
+    resize_count = 0
 
     cap = cv2.VideoCapture("capture.avi")
     while(cap.isOpened()):
         ret, frame = cap.read()
         if ret == False:
             break
-        frameIndex = int(totalFrames / config.native_fps)
+        frame_index = int(total_frames / config.native_fps)
         resized = resizeImage(frame)
-        cv2.imwrite(os.path.join(path, "img{}.jpg".format(frameIndex)), resized)
-        totalFrames += config.native_fps
-        cap.set(config.fps, totalFrames)
-        resizeCount+=1
-        resizeProgress(resizeCount, int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))
+        cv2.imwrite(os.path.join(path, "img{}.jpg".format(frame_index)), resized)
+        total_frames += config.native_fps
+        cap.set(config.fps, total_frames)
+        resize_count+=1
+        resizeProgress(resize_count, int(cap.get(cv2.CAP_PROP_FRAME_COUNT)))
 
     
     cap.release()
@@ -39,9 +42,9 @@ def getFrames():
 def resizeImage(img):
     return cv2.resize(img, config.scaled_res, interpolation = cv2.INTER_AREA)
 
-def resizeProgress(currentFrame, totalFrames):
-    totalOutputFrames = int(totalFrames * (config.fps/config.native_fps))
-    progress = round((currentFrame / totalOutputFrames * 100), 2)
+def resizeProgress(current_frame, total_frames):
+    total_output_frames = int((total_frames / config.native_fps) * config.fps)
+    progress = round((current_frame / total_output_frames * 100), 2)
     sys.stdout.write('\r')
-    sys.stdout.write("{}/{} - {}%".format(currentFrame, totalOutputFrames, progress))
+    sys.stdout.write("Resizing: {}/{} - {}%".format(current_frame, total_output_frames, progress))
     sys.stdout.flush
